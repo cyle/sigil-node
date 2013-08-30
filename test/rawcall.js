03074ca5e1155c8d0d01dd531182bdf86f091815
Cyle <cylegage@gmail.com>
2013-08-30 16:36:28 -0400

    - the rawCall method works, now to build those helper functions...

--- test/rawcall.js
+++ test/rawcall.js
@@ -2,6 +2,20 @@
 var sigil = require('../lib/sigil-node.js');
 
 // create an instance of the client
-var db = new sigil('localhost');
+var db = new sigil;
 
-db.rawCall('/');
+db.rawCall('/node', 'POST', {'Name':'a new node!'}, function(err, result) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('the new node ID: ' + result);
+	}
+});
+
+db.rawCall('/nodes', function(err, result) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(result);
+	}
+});
